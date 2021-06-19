5a181d012d9ea4e7ee3f398cff0c765e80f97738
Fialkaksu <fialkaksu1984@gmail.com>
2021-06-19 20:11:51 +0300

    feat: add header and switchTheme handler

--- js/script.js
+++ js/script.js
@@ -1,15 +1,35 @@
-const lightToggles = document.querySelectorAll('.toggle-light');
-const darkToggles = document.querySelectorAll('.toggle-dark');
-const backendlessToggles = document.querySelectorAll('.toggle-backendless');
-
-const radioToggles = document.querySelectorAll('.theme__input[type="radio"]')
+const radioToggles = document.querySelectorAll('.theme__toggle[type="radio"]');
+const buttonToggles = document.querySelectorAll('button.theme__toggle');
+const selectToggles = selectTheme;
 
 Array.from(radioToggles).forEach(el => {
   el.addEventListener('click', switchTheme);
 });
+Array.from(buttonToggles).forEach(el => {
+  el.addEventListener('click', switchTheme);
+});
+selectToggles.addEventListener('change', () => {
+  let index = selectToggles.selectedIndex;
+  let theme;
+  switch (index) {
+    case 0:
+      theme = 'light';
+      break;
+    case 1:
+      theme = 'dark';
+      break;
+    case 2:
+      theme = 'backendless';
+      break;
+    default:
+      theme = 'light';
+  }
+  console.log("Switching theme to " + theme);
+  document.documentElement.setAttribute('theme', theme);
+});
 
-function switchTheme(e){
-  let theme=e.target.dataset.theme;
-  console.log("Switching theme to "+ theme);
+function switchTheme(e) {
+  let theme = e.target.dataset.theme;
+  console.log("Switching theme to " + theme);
   document.documentElement.setAttribute('theme', theme);
 }
