842a6741a08cda53564aff31934d582a3898dee8
Fialkaksu <fialkaksu1984@gmail.com>
2021-06-21 18:00:42 +0300

    feat: add gallery

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const radioToggles = document.querySelectorAll('.theme__toggle[type="radio"]');
 const buttonToggles = document.querySelectorAll('button.theme__toggle');
 const selectToggles = selectTheme;
+const elementsToInvert = document.querySelectorAll('.gallery__item');
+// console.log(elementsToInvert);
+let theme;
 
 Array.from(radioToggles).forEach(el => {
   el.addEventListener('click', switchTheme);
@@ -10,7 +13,7 @@ Array.from(buttonToggles).forEach(el => {
 });
 selectToggles.addEventListener('change', () => {
   let index = selectToggles.selectedIndex;
-  let theme;
+  // let theme;
   switch (index) {
     case 0:
       theme = 'light';
@@ -24,12 +27,24 @@ selectToggles.addEventListener('change', () => {
     default:
       theme = 'light';
   }
-  console.log("Switching theme to " + theme);
+  // console.log("Switching theme to " + theme);
   document.documentElement.setAttribute('theme', theme);
+  checkInvert();
 });
 
+function checkInvert() {
+  Array.from(elementsToInvert).forEach(el => {
+    if (theme === 'backendless') {
+      el.classList.add('invert');
+    } else {
+      el.classList.remove('invert');
+    }
+  });
+}
+
 function switchTheme(e) {
-  let theme = e.target.dataset.theme;
-  console.log("Switching theme to " + theme);
+  theme = e.target.dataset.theme;
+  // console.log("Switching theme to " + theme);
   document.documentElement.setAttribute('theme', theme);
+  checkInvert();
 }
